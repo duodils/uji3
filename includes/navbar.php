2ac2f38daa217875f58b995b12aefae491567778
duodils <epimpim8@gmail.com>
2024-08-29 07:40:32 +0700

    Menambahkan halaman index, dan navbar

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav class="nav">
+    <div class="menu-icon"></div>
+    <img src="asset/img/logo.png" alt="" class="logo">
+    <ul class="ul-item">
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Ticket</a></li>
+        <li><a href="">Layout</a></li>
+        <li><a href="">Gallery</a></li>
+    </ul>
+    <img src="asset/img/user.png" alt="" class="user">
+</nav>
