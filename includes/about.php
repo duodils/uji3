f91c2be3c4f25c61fc92f5036a0a7582b8ed9ca7
duodils <epimpim8@gmail.com>
2024-08-29 12:38:48 +0700

    Memperbaiki tampilan

---
+++ includes/about.php
@@ -0,0 +1,43 @@
+<div class="" id="about" style="padding-top: 20px;">
+    <div class="about">
+        <img src="asset/img/image.png" alt="">
+        <div class="content-about">
+            <h2 class="judul" style="font-weight: 900; border-bottom: yellow 1px solid; width: 250px;">ABOUT CONCERT</h2>
+            <h1 style="font-weight: 900;">BRUNO MARS TOUR FOR THE LATEST ALBUM - '24K MAGIC'</h1>
+            <p>Bruno Mars will light up Jakarta International Stadium on September 13th and 14th, 2024, as part of his world
+                tour. Experience his electrifying performances of chart-topping hits like "Uptown Funk" and "Just the Way
+                You Are." This is your chance to witness a spectacular evening filled with energy, music, and Bruno Mars'
+                signature showmanship. Get your tickets now on the official Bruno on Stage (BOS) website before they sell
+                out!
+            </p>
+            <div class="more" style="display: grid; grid-template-columns: repeat(3, 300px); justify-content: space-between;">
+                <div>
+                    <h4>Place</h4>
+                    <p>Jakarta International Stadium. Papanggo, Kec. Tj. Priok, Jkt Utara, Daerah Khusus Ibukota Jakarta</p>
+                </div>
+                <div>
+                    <h4>Date</h4>
+                    <p>September 13th, September 14th 2024</p>
+                </div>
+                <div>
+                    <h4>Time</h4>
+                    <p>10.00PM - 1.00AM</p>
+                </div>
+            </div>
+            <hr>
+            <h2 style="text-align: center;">Another Information</h2>
+            <div class="info-about">
+                <button>Terms</button>
+                <button>Safety</button>
+                <button>Age</button>
+                <button>Security</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal"  id="modalhowtoorder">
+    <div class="modal-content" style="width: 800px;" >
+        
+    </div>
+</div>
+</div>
--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-<div style="background-color: blue; padding: 20px 30px;">
-    <div style="display: flex; height: 200px; align-items: start; margin-top: 20px; justify-content:space-around;">
+<div style="background-color: black; padding: 20px 30px;">
+    <div style="display: flex; height: 200px; align-items: start; font-family: 'Montserrat', serif; margin-top: 20px; justify-content:space-around;">
         <img src="asset/img/logo.png" alt="" style="width: 230px;">
         <ul style="color:#fff;">
             <h2>Navigation</h2>
@@ -30,8 +30,12 @@
             </li>
         </ul>
     </div>
-    <div style="margin: 20px 160px;">
-<p style="color: #fff;"> Copyright &copy; dilsdev & dkjna on ALPHA 2024 - All right reserved</p>
-
+    <div style="margin: 20px 160px; border-top: yellow 1px solid; font-family: 'Montserrat', serif; padding-top: 10px; display: flex; justify-content: space-between;">
+        <p style="color: #fff;"> Copyright &copy; dilsdev & dkjna on ALPHA 2024 - All right reserved</p>
+        <div class="icon">
+            <img src="asset/img/youtube.png" style="width: 35px;" alt="">
+            <img src="asset/img/youtube.png" style="width: 35px;" alt="">
+            <img src="asset/img/youtube.png" style="width: 35px;" alt="">
+        </div>
     </div>
 </div>
